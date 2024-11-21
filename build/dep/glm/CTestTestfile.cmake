2c2644ca49f06ab78bda363e6b96cf1e6ec593d4
Telo PHILIPPE <telo.philippe@gmail.com>
2024-11-21 15:51:00 +0100

    Updated to work with cmake

---
+++ build/dep/glm/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/telop/git_projects/TestBuffers/dep/glm
+# Build directory: C:/Users/telop/git_projects/TestBuffers/build/dep/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
