a0b3d3efdcca9ce18ff4b44fe93d70981fb1c8e7
Telo PHILIPPE <telo.philippe@gmail.com>
2024-12-01 16:18:26 +0100

    Removed build

--- build/dep/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/telop/git_projects/TestBuffers/dep/glm/glm
-# Build directory: C:/Users/telop/git_projects/TestBuffers/build/dep/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
